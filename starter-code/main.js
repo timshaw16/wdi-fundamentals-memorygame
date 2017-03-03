5d88fc2fb637f49028507abe575ac852c5195abe
Tim <tim@Tims-MacBook-Air.local>
2017-03-03 19:57:49 +0000

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+/*if (cardTwo === cardThree) {
+    alert("You found a match!");
+} else {
+    alert("Not a match, try again!");
+}*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i=0; i<4; i++){	
+var cardsElement = document.createElements('div');
+		cardsElement.className = 'card';
+		gameBoard.appendChild(gameBoard);
+	}
+
+}
